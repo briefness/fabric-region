81ec55d1a2478b9982d1b91ed73e24e33bbcbe70
briefness <920178114@qq.com>
2023-12-15 17:21:50 +0800

    清空

--- src/main.js
+++
@@ -1,7 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import antDesignVue from 'ant-design-vue';
-import 'ant-design-vue/dist/antd.css'; // or 'ant-design-vue/dist/antd.less'
-import './index.css'
-
-createApp(App).use(antDesignVue).mount('#app')
